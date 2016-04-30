21fe5907eb1127bde8bfb428a6fd339d5c8c70f3
canoi12 <canoimaster@gmail.com>
2016-04-30 18:12:18 -0300

    1

--- ZombiesAteMyNeigh(Lua TOP)/conf.lua
+++
@@ -1,7 +0,0 @@
-function love.conf(t)
-    t.window.width = 640
-    t.window.height = 640
-    t.window.minwidth = 64
-    t.window.minheight = 64
-    t.title = "Zombies ate my Neigh... Wait!"
-end
